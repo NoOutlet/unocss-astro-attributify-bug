bbd40e8f665464d005e51a12bda6f2a9001a8dc8
Squiggs Anderson <squiggs.jonny5@gmail.com>
2025-12-14 12:34:54 -0500

    Add UnoCSS with Attributify

---
+++ uno.config..ts
@@ -0,0 +1,11 @@
+import { defineConfig } from "unocss";
+import presetAttributify from "@unocss/preset-attributify";
+
+export default defineConfig({
+  presets: [
+    presetAttributify({
+      /* preset options */
+    }),
+    // ...
+  ],
+});
