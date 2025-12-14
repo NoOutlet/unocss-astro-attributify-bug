7d79c10419d930172ef9f37e755c485bf10153bf
Squiggs Anderson <squiggs.jonny5@gmail.com>
2025-12-14 15:00:25 -0500

    Add breakpoints; test attributes

--- uno.config.ts
+++ uno.config.ts
@@ -1,5 +1,19 @@
 import { defineConfig, presetAttributify, presetWind3 } from "unocss";
 
+const breakpoints = {
+  "2xs": "320px",
+  xs: "375px",
+  sm: "480px",
+  tablet: "600px",
+  md: "768px",
+  lg: "1024px",
+  desktop: "1280px",
+  xl: "1440px",
+  "2xl": "1600px", // max content width
+  "3xl": "1920px",
+  "4xl": "2560px",
+};
+
 export default defineConfig({
   content: {
     pipeline: {
@@ -7,4 +21,7 @@ export default defineConfig({
     },
   },
   presets: [presetWind3(), presetAttributify()],
+  theme: {
+    breakpoints,
+  },
 });
